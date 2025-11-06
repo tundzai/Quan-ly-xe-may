02daefaf87e84db1c3327ba572c1cc535d734985
tundzai <152704464+tundzai@users.noreply.github.com>
2025-11-06 12:54:43 +0700

    Add files via upload

---
+++ BT01.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    freopen("BT01.INP","r",stdin);
+    freopen("BT01.OUT","w",stdout);
+    int n,t=0;
+    cin>>n;
+    t=n*(n+1)/2;
+    cout<<t;
+}
